5a3820aee9c9202a0ee70d811d9bcd2d9f58aa5d
Markus Schaden <mrksschdn@gmail.com>
2016-10-17 15:45:23 +0200

    Added MonitorWaitOrSignal Smell

---
+++ ConcurrencyChecker/ConcurrencyChecker/ConcurrencyChecker.Test/MonitorWaitOrSignal/MonitorWaitOrSignalUnitTests.cs
@@ -0,0 +1,353 @@
+﻿using ConcurrencyChecker.ExplicitThreadsChecker;
+using ConcurrencyChecker.MonitorWaitOrSignal;
+using ExplicitThreadsChecker;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestHelper;
+
+namespace ConcurrencyChecker.Test.MonitorWaitOrSignal
+{
+    [TestClass]
+    public class MwsUnitTest : CodeFixVerifier
+    {
+
+        
+        [TestMethod]
+        public void TestNoDiagnostics()
+        {
+            var test = @"
+using System.Threading;
+
+namespace MonitorWaitOrSignalSmell
+{
+    class BoundedBuffer<T>
+    {
+        private Queue<T> queue = new Queue<T>();
+        private const int Limit = 1;
+        public void Put(T x)
+        {
+            lock (this)
+            {
+                while (queue.Count == Limit)
+                {
+                    Monitor.Wait(this);
+                }
+                queue.Enqueue(x);
+                Monitor.Pulse(this); // signal non-free
+             }
+        }
+    }
+}";
+            VerifyCSharpDiagnostic(test);
+        }
+
+
+        [TestMethod]
+        public void TestSimpleIfDiagnostics()
+        {
+            var test = @"
+using System.Threading;
+
+namespace MonitorWaitOrSignalSmell
+{
+    class BoundedBuffer<T>
+    {
+        private Queue<T> queue = new Queue<T>();
+        private const int Limit = 2;
+        public void Put(T x)
+        {
+            lock (this)
+            {
+                if (queue.Count == Limit)
+                {
+                    Monitor.Wait(this);
+                }
+                queue.Enqueue(x);
+                Monitor.PulseAll(this); // signal non-free
+            }
+        }
+        public T Get()
+        {
+            lock (this)
+            {
+                if (queue.Count == 0)
+                {
+                    Monitor.Wait(this);
+                }
+                T x = queue.Dequeue();
+                Monitor.PulseAll(this); // signal non-full
+                return x;
+            }
+        }
+    }
+}";
+
+            var expected1 = new DiagnosticResult
+            {
+                Id = "MWS001",
+                Message = "if should be replaced with while",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 14, 17),
+                        }
+            };
+
+            var expected2 = new DiagnosticResult
+            {
+                Id = "MWS001",
+                Message = "if should be replaced with while",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 26, 17),
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected1, expected2);
+        }
+
+        [TestMethod]
+        public void TestSimplePulseDiagnostics()
+        {
+            var test = @"
+using System.Threading;
+
+namespace MonitorWaitOrSignalSmell
+{
+    class BoundedBuffer<T>
+    {
+        private Queue<T> queue = new Queue<T>();
+        private const int Limit = 2;
+        public void Put(T x)
+        {
+            lock (this)
+            {
+                while (queue.Count == Limit)
+                {
+                    Monitor.Wait(this);
+                }
+                queue.Enqueue(x);
+                Monitor.Pulse(this); // signal non-free
+            }
+        }
+        public T Get()
+        {
+            lock (this)
+            {
+                while (queue.Count == 0)
+                {
+                    Monitor.Wait(this);
+                }
+                T x = queue.Dequeue();
+                Monitor.Pulse(this); // signal non-full
+                return x;
+            }
+        }
+    }
+}";
+
+            var expected1 = new DiagnosticResult
+            {
+                Id = "MWS002",
+                Message = "Pulse should be replaced with PulseAll",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 19, 17),
+                        }
+            };
+
+            var expected2 = new DiagnosticResult
+            {
+                Id = "MWS002",
+                Message = "Pulse should be replaced with PulseAll",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 31, 17),
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected1, expected2);
+        }
+
+
+        [TestMethod]
+        public void TestPulseReplacement()
+        {
+            var test = @"
+using System.Threading;
+
+namespace MonitorWaitOrSignalSmell
+{
+    class BoundedBuffer<T>
+    {
+        private Queue<T> queue = new Queue<T>();
+        private const int Limit = 2;
+        public void Put(T x)
+        {
+            lock (this)
+            {
+                while (queue.Count == Limit)
+                {
+                    Monitor.Wait(this);
+                }
+                queue.Enqueue(x);
+                Monitor.Pulse(this); // signal non-free
+            }
+        }
+        public T Get()
+        {
+            lock (this)
+            {
+                while (queue.Count == 0)
+                {
+                    Monitor.Wait(this);
+                }
+                T x = queue.Dequeue();
+                Monitor.Pulse(this); // signal non-full
+                return x;
+            }
+        }
+    }
+}";
+
+            var fixtest = @"
+using System.Threading;
+
+namespace MonitorWaitOrSignalSmell
+{
+    class BoundedBuffer<T>
+    {
+        private Queue<T> queue = new Queue<T>();
+        private const int Limit = 2;
+        public void Put(T x)
+        {
+            lock (this)
+            {
+                while (queue.Count == Limit)
+                {
+                    Monitor.Wait(this);
+                }
+                queue.Enqueue(x);
+                Monitor.PulseAll(this); // signal non-free
+            }
+        }
+        public T Get()
+        {
+            lock (this)
+            {
+                while (queue.Count == 0)
+                {
+                    Monitor.Wait(this);
+                }
+                T x = queue.Dequeue();
+                Monitor.PulseAll(this); // signal non-full
+                return x;
+            }
+        }
+    }
+}";
+            VerifyCSharpFix(test, fixtest, allowNewCompilerDiagnostics: true);
+        }
+
+
+        [TestMethod]
+        public void TestIfReplacement()
+        {
+            var test = @"
+using System.Threading;
+
+namespace MonitorWaitOrSignalSmell
+{
+    class BoundedBuffer<T>
+    {
+        private Queue<T> queue = new Queue<T>();
+        private const int Limit = 2;
+        public void Put(T x)
+        {
+            lock (this)
+            {
+                if (queue.Count == Limit)
+                {
+                    Monitor.Wait(this);
+                }
+                queue.Enqueue(x);
+                Monitor.PulseAll(this); // signal non-free
+            }
+        }
+        public T Get()
+        {
+            lock (this)
+            {
+                if (queue.Count == 0)
+                {
+                    Monitor.Wait(this);
+                }
+                T x = queue.Dequeue();
+                Monitor.PulseAll(this); // signal non-full
+                return x;
+            }
+        }
+    }
+}";
+
+            var fixtest = @"
+using System.Threading;
+
+namespace MonitorWaitOrSignalSmell
+{
+    class BoundedBuffer<T>
+    {
+        private Queue<T> queue = new Queue<T>();
+        private const int Limit = 2;
+        public void Put(T x)
+        {
+            lock (this)
+            {
+                while (queue.Count == Limit)
+                {
+                    Monitor.Wait(this);
+                }
+                queue.Enqueue(x);
+                Monitor.PulseAll(this); // signal non-free
+            }
+        }
+        public T Get()
+        {
+            lock (this)
+            {
+                while (queue.Count == 0)
+                {
+                    Monitor.Wait(this);
+                }
+                T x = queue.Dequeue();
+                Monitor.PulseAll(this); // signal non-full
+                return x;
+            }
+        }
+    }
+}";
+            VerifyCSharpFix(test, fixtest, allowNewCompilerDiagnostics: true);
+        }
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider()
+        {
+            return new MonitorWaitOrSignalCodeFixProvider();
+        }
+
+        protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer()
+        {
+            return new MonitorWaitOrSignalAnalyzer();
+        }
+
+        
+    }
+
+
+
+
+}
---
+++ ConcurrencyChecker/ConcurrencyChecker/ConcurrencyChecker/MonitorWaitOrSignal/MonitorWaitOrSignalCodeFixProvider.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using ConcurrencyChecker.ExplicitThreadsChecker;
+using ExplicitThreadsChecker;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace ConcurrencyChecker.MonitorWaitOrSignal
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MonitorWaitOrSignalCodeFixProvider)), Shared]
+    public class MonitorWaitOrSignalCodeFixProvider : CodeFixProvider
+    {
+        private const string Title = "Use if instead of while";
+
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+            => ImmutableArray.Create(MonitorWaitOrSignalAnalyzer.MonitorIfConditionDiagnosticId, MonitorWaitOrSignalAnalyzer.MonitorPulseDiagnosticId);
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            var diagnostic = context.Diagnostics.First();
+
+            var node = root.FindNode(context.Span);
+
+            if (node is InvocationExpressionSyntax)
+            {
+                context.RegisterCodeFix(
+                 CodeAction.Create(Title, c => ReplacePulseWithPulseAll(context.Document, (InvocationExpressionSyntax)node, c), Title),
+                 diagnostic);
+            }
+            else if (node is IfStatementSyntax)
+            {
+                context.RegisterCodeFix(
+                 CodeAction.Create(Title, c => ReplaceIfWithWhile(context.Document, (IfStatementSyntax)node, c), Title),
+                 diagnostic);
+            }
+
+            
+        }
+
+        private async Task<Document> ReplaceIfWithWhile(Document document, IfStatementSyntax node, CancellationToken cancellationToken)
+        {
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+            var whileLoop = SyntaxFactory.WhileStatement(node.Condition, node.Statement);
+
+            var newRoot = root.ReplaceNode(node, whileLoop);
+
+            var newDocument = document.WithSyntaxRoot(newRoot);
+
+            return newDocument;
+        }
+
+        private async Task<Document> ReplacePulseWithPulseAll(Document document, InvocationExpressionSyntax node, CancellationToken cancellationToken)
+        {
+            var root = await document.GetSyntaxRootAsync(cancellationToken);
+
+            var identifier = node.DescendantNodes().OfType<IdentifierNameSyntax>().First(e => e.Identifier.ToString() == "Pulse");
+
+            var pulseAll = SyntaxFactory.IdentifierName("PulseAll");
+            
+            var newRoot = root.ReplaceNode(identifier, pulseAll);
+            var newDocument = document.WithSyntaxRoot(newRoot);
+
+            return newDocument;
+        }
+        
+    }
+}
