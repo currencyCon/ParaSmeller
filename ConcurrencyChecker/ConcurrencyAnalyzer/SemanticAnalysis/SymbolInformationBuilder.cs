ee388c08a4bacbacfea078bad3d3945766e64f6c
Markus Schaden <mrksschdn@gmail.com>
2016-11-24 10:15:12 +0100

    Fixed nullpointer exception

--- ConcurrencyChecker/ConcurrencyAnalyzer/SemanticAnalysis/SymbolInformationBuilder.cs
+++ ConcurrencyChecker/ConcurrencyAnalyzer/SemanticAnalysis/SymbolInformationBuilder.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace ConcurrencyAnalyzer.SemanticAnalysis
@@ -10,13 +11,26 @@ namespace ConcurrencyAnalyzer.SemanticAnalysis
         {
             var symbolInfo = semanticModel.GetSymbolInfo(simpleNameSyntax);
             var symbol = symbolInfo.Symbol;
-            return new SymbolInformation
+            try
             {
-                ClassName = symbol == null? DefaultSymbolValues.ClassName : GetClassName(symbol),
-                OriginalDefinition = symbol == null ? DefaultSymbolValues.OriginalDefinition : GetOriginalDefinition(symbol),
-                Definition = symbol == null ? DefaultSymbolValues.Definition : symbol.OriginalDefinition.ToString(),
-                Type = symbol == null ? DefaultSymbolValues.Type : GetType(symbol)
-            };
+                return new SymbolInformation
+                {
+                    ClassName = symbol == null ? DefaultSymbolValues.ClassName : GetClassName(symbol),
+                    OriginalDefinition = symbol == null ? DefaultSymbolValues.OriginalDefinition : GetOriginalDefinition(symbol),
+                    Definition = symbol == null ? DefaultSymbolValues.Definition : symbol.OriginalDefinition.ToString(),
+                    Type = symbol == null ? DefaultSymbolValues.Type : GetType(symbol)
+                };
+            }
+            catch (Exception)
+            {
+                return new SymbolInformation()
+                {
+                    ClassName = DefaultSymbolValues.ClassName,
+                    OriginalDefinition = DefaultSymbolValues.OriginalDefinition,
+                    Definition = DefaultSymbolValues.Definition,
+                    Type = DefaultSymbolValues.Type
+                };
+            }
         }
         
         private static SymbolKind GetType(ISymbol symbol)
@@ -28,6 +42,7 @@ namespace ConcurrencyAnalyzer.SemanticAnalysis
         {
             return symbol.ContainingType.Name;
         }
+
         private static string GetOriginalDefinition(ISymbol symbol)
         {
             return symbol.ContainingType.OriginalDefinition + NameSpaceSepatator + symbol.Name;
