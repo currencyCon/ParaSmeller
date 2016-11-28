0c19f5392a27c3fb66570840cc376932f0d137f4
Markus Schaden <mrksschdn@gmail.com>
2016-11-28 09:34:43 +0100

    Renamed projects

--- ParaSmeller/ParaSmellerAnalyzer/ParaSmellerAnalyzer/Properties/AssemblyInfo.cs
+++ ParaSmeller/ParaSmellerAnalyzer/ParaSmellerAnalyzer/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConcurrencyChecker")]
+[assembly: AssemblyTitle("ParaSmeller")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConcurrencyChecker")]
+[assembly: AssemblyProduct("ParaSmeller")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
