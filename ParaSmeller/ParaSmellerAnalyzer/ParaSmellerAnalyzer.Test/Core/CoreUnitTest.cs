7165d4baa4c954cc2f8f16e8fb69964d2a47713b
Markus Schaden <mrksschdn@gmail.com>
2016-12-12 09:44:21 +0100

    Core testing

---
+++ ParaSmeller/ParaSmellerAnalyzer/ParaSmellerAnalyzer.Test/Core/CoreUnitTest.cs
@@ -0,0 +1,147 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ParaSmeller.Test.Core
+{
+    [TestClass]
+    public class CoreUnitTest
+    {
+
+        [TestMethod]
+        public void TestSimpleSubClasses()
+        {
+            const string test = @"
+namespace ParaSmeller.Test.Core
+{
+    public class Base
+    {
+        
+    }
+
+    public class Sub : Base
+    {
+        
+    }
+
+    public class SubSub : Sub
+    {
+        
+    }
+}";
+            var solution = TestSolutionBuilder.CreateSolution(test);
+            Assert.AreEqual(3, solution.Classes.Count);
+            Assert.AreEqual(3, solution.ClassMap.Count);
+
+            Assert.AreEqual(0, solution.ClassMap["ParaSmeller.Test.Core.Base"].ToArray()[0].ClassMap.Count);
+            Assert.AreEqual(1, solution.ClassMap["ParaSmeller.Test.Core.Sub"].ToArray()[0].ClassMap.Count);
+            Assert.AreEqual(2, solution.ClassMap["ParaSmeller.Test.Core.SubSub"].ToArray()[0].ClassMap.Count);
+
+            Assert.AreEqual(2, solution.Classes.ToArray()[0].ClassMap.Count);
+            Assert.AreEqual(1, solution.Classes.ToArray()[1].ClassMap.Count);
+            Assert.AreEqual(0, solution.InterfaceMap.Count);
+        }
+
+
+        [TestMethod]
+        public void TestSimpleInterfaces()
+        {
+            const string test = @"
+namespace ParaSmeller.Test.Core
+{
+    public interface IBase 
+    {
+
+    }
+
+    public class Base : IBase
+    {
+        
+    }
+
+    public class Sub : Base, ISub
+    {
+        
+    }
+
+    public interface ISub
+    {
+        
+    }
+}";
+            var solution = TestSolutionBuilder.CreateSolution(test);
+            Assert.AreEqual(2, solution.Classes.Count);
+            Assert.AreEqual(2, solution.ClassMap.Count);
+            Assert.AreEqual(2, solution.InterfaceMap.Count);
+
+            Assert.AreEqual(2, solution.InterfaceMap["ParaSmeller.Test.Core.IBase"].ImplementingClasses.Count);
+            Assert.AreEqual(1, solution.InterfaceMap["ParaSmeller.Test.Core.ISub"].ImplementingClasses.Count);
+
+            Assert.AreEqual(1, solution.ClassMap["ParaSmeller.Test.Core.Sub"].ToArray()[0].ClassMap.Count);
+
+            //Assert.AreEqual(2, solution.Classes.ToArray()[0].ClassMap.Count);
+            //Assert.AreEqual(1, solution.Classes.ToArray()[1].ClassMap.Count);
+
+        }
+
+
+        [TestMethod]
+        public void TestPartialClasses()
+        {
+            const string test = @"
+namespace ParaSmeller.Test.Core
+{
+    public partial class Base
+    {
+        public void Method1() {}
+    }
+
+    public partial class Base
+    {
+        public void Method2() {}
+    }
+
+}";
+            var solution = TestSolutionBuilder.CreateSolution(test);
+            Assert.AreEqual(2, solution.Classes.Count);
+            Assert.AreEqual(1, solution.ClassMap.Count);
+            Assert.AreEqual(2, solution.ClassMap["ParaSmeller.Test.Core.Base"].Count);
+        }
+
+        [TestMethod]
+        public void TestInvocations()
+        {
+            const string test = @"
+namespace ParaSmeller.Test.Core
+{
+    public class A
+    {
+        public void Method1() 
+        {
+            B b = new B();
+            b.Method2();
+        }
+    }
+
+    public class B
+    {
+        public void Method2() {}
+    }
+
+}";
+            var solution = TestSolutionBuilder.CreateSolution(test);
+            Assert.AreEqual(2, solution.Classes.Count);
+            Assert.AreEqual(2, solution.ClassMap.Count);
+
+            var method1 = (solution.ClassMap["ParaSmeller.Test.Core.A"].ToArray()[0].Methods.ToArray()[0]);
+            Assert.IsNotNull(method1);
+            Assert.AreEqual(1, method1.Blocks.Count);
+            Assert.AreEqual(1, method1.Blocks.ToArray()[0].InvocationExpressions.Count);
+            var invocationMethodB = method1.Blocks.ToArray()[0].InvocationExpressions.ToArray()[0];
+            Assert.AreEqual("ParaSmeller.Test.Core.B", invocationMethodB.CalledClassOriginal);
+            Assert.AreEqual("ParaSmeller.Test.Core.B.Method2()", invocationMethodB.OriginalDefinition);
+
+        }
+
+
+    }
+}
---
+++ ParaSmeller/ParaSmellerAnalyzer/ParaSmellerAnalyzer.Test/Core/TestSolutionBuilder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
+using ParaSmellerCore.Representation;
+using ParaSmellerCore.RepresentationFactories;
+
+namespace ParaSmeller.Test.Core
+{
+    public static class TestSolutionBuilder
+    {
+        private static readonly MetadataReference CorlibReference = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+        private static readonly MetadataReference SystemCoreReference = MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location);
+        private static readonly MetadataReference CSharpSymbolsReference = MetadataReference.CreateFromFile(typeof(CSharpCompilation).Assembly.Location);
+        private static readonly MetadataReference CodeAnalysisReference = MetadataReference.CreateFromFile(typeof(Compilation).Assembly.Location);
+
+        private static string DefaultFilePathPrefix = "Test";
+        private static string CSharpDefaultFileExt = "cs";
+        private static string VisualBasicDefaultExt = "vb";
+        private static string TestProjectName = "TestProject";
+        
+        public static SolutionRepresentation CreateSolution(string source)
+        {
+            var project = CreateProject(new[] { source });
+            var compilation = project.GetCompilationAsync().Result;
+            var solution = SolutionRepresentationFactory.Create(compilation);
+            return solution;
+        }
+
+        private static Project CreateProject(string[] sources, string language = LanguageNames.CSharp)
+        {
+            string fileNamePrefix = DefaultFilePathPrefix;
+            string fileExt = language == LanguageNames.CSharp ? CSharpDefaultFileExt : VisualBasicDefaultExt;
+
+            var projectId = ProjectId.CreateNewId(debugName: TestProjectName);
+
+            var solution = new AdhocWorkspace()
+                .CurrentSolution
+                .AddProject(projectId, TestProjectName, TestProjectName, language)
+                .AddMetadataReference(projectId, CorlibReference)
+                .AddMetadataReference(projectId, SystemCoreReference)
+                .AddMetadataReference(projectId, CSharpSymbolsReference)
+                .AddMetadataReference(projectId, CodeAnalysisReference);
+
+            int count = 0;
+            foreach (var source in sources)
+            {
+                var newFileName = fileNamePrefix + count + "." + fileExt;
+                var documentId = DocumentId.CreateNewId(projectId, debugName: newFileName);
+                solution = solution.AddDocument(documentId, newFileName, SourceText.From(source));
+                count++;
+            }
+            return solution.GetProject(projectId);
+        }
+
+    }
+}
